0ea14db443d9e0a0d1e3fd665627b5509b9e3001
blitterated <blitterated@protonmail.com>
2026-08-11 22:12:20 -0500

    Code cleanup with shellcheck

--- manual-test-examples.sh
+++ manual-test-examples.sh
@@ -21,15 +21,15 @@ show_bg_color_for_each_fg_color 1 3 5
 
 # some functions to help isolate different calls to cycle_216_8bit_colors_rgb in the call stack
 function test_cycle_1 () {
-  x1=$1; y1=$2; z1=$3;
-  printf "test_cycle_1 %s %s %s\\n" $x1 $y1 $z1
+  local x1=$1; y1=$2; z1=$3;
+  printf "test_cycle_1 %s %s %s\\n" "$x1" "$y1" "$z1"
   cycle_216_8bit_colors_rgb "test_cycle_2 $x1 $y1 $z1"
 }
 
 function test_cycle_2 () {
-  x1=$1; y1=$2; z1=$3;
-  x2=$4; y2=$5; z2=$6;
-  printf "    test_cycle_2 %s %s %s %s %s %s\\n" $x1 $y1 $z1 $x2 $y2 $z2
+  local x1="$1" y1="$2" z1="$3"
+  local x2="$4" y2="$5" z2="$6"
+  printf "    test_cycle_2 %s %s %s %s %s %s\\n" "$x1" "$y1" "$z1" "$x2" "$y2" "$z2"
 }
 
 cycle_216_8bit_colors_rgb 'test_cycle_1'
@@ -53,4 +53,5 @@ another_xyz_loop 'test_cycle_1'
 # a real test of cycle_216_8bit_colors_rgb
 cycle_216_8bit_colors_rgb "show_bg_color_for_each_fg_color"
 
-
+# show all color combinations
+show_colors
